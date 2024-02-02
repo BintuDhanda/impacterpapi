4b982b5e09265b6ff747fbc8f46514a7a4bf0c71
BALWANT SINGH <bintudhanda@outlook.com>
2024-02-02 19:45:50 +0530

    Hostel Room Student Interface and Model added

---
+++ Interface/HostelRoomStudent.cs
@@ -0,0 +1,14 @@
+﻿using ERP.Models;
+
+namespace ERP.Interface
+{
+    public interface IHostelRoomStudent
+    {
+        Task<IEnumerable<HostelRoomStudent>> GetAllAsync();
+        Task<Account> GetByIdAsync(int Id);
+        Task<Account> AddAsync(HostelRoomStudent account);
+        Task<Account> UpdateAsync(HostelRoomStudent account);
+        Task<Account> DeleteAsync(int Id);
+        Task<IEnumerable<HostelRoomStudent>> GetAccountByAccountCategoryIdAsync(int Id);
+    }
+}
