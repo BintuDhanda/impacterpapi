e657766ec4d1aa777b26a1985ddebb5b9e87e561
Dharm Veer <109572838+dharmveerofficial@users.noreply.github.com>
2024-02-03 14:27:49 +0530

    03-023-2024

--- obj/Debug/net6.0/ERP.AssemblyInfo.cs
+++ obj/Debug/net6.0/ERP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6595492ed8f036c8c0ebbb391f288540c4aaab27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+212fb4ddb587c86b3d8bd4b9d85cb3ce774b4827")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
