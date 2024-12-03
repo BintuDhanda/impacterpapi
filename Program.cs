0d786f0f6b20ccfa58b713c4b31860723636f22e
dharmveerofficial <dharmvirofficial@gmail.com>
2024-12-03 10:49:43 +0530

    updated

--- Program.cs
+++ Program.cs
@@ -11,8 +11,10 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.UseUrls(
-    "http://0.0.0.0:5000",
-    "https://0.0.0.0:5001"
+    //"http://0.0.0.0:5000",
+    //"https://0.0.0.0:5001",
+    "http://0.0.0.0:5002",
+    "https://0.0.0.0:5003"
     );
 // Add services to the container.
 var env = builder.Configuration["EnvironmentVariable"];
